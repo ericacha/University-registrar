e98d6de24b98d69f0d4460f6d501e10aab35a02f
Erica Cha Vang <laujmimna@Ericas-MacBook-Air.local>
2015-03-24 21:31:10 -0700

    The testSetName pass.

--- tests/StudentTest.php
+++ tests/StudentTest.php
@@ -27,21 +27,23 @@
             //Assert
             $this->assertEquals($name,$result);
 
-      //   }
-      //   function testSetName()
-      //   {
-      //       //Arrange
-      //       $name = "Tommy";
-      //       $test_student = new Student($name);
-      //
-      //       //Act
-      //       $test_student->setName("Cool.");
-      //       $result = $test_student->getName();
-      //
-      //       //Assert
-      //       $this->assertEquals("Cool.",$result);
-      //
-      //   }
+        }
+        function testSetName()
+        {
+            //Arrange
+            $name = "Tommy";
+            $date = 2000-03-23;
+            $id = null;
+            $test_student = new Student($name, $date, $id);
+
+            //Act
+            $test_student->setName("Cool.");
+            $result = $test_student->getName();
+
+            //Assert
+            $this->assertEquals("Cool.",$result);
+
+        }
       //
       //   function testGetId()
       //
@@ -51,7 +53,7 @@
       // }
 
 
-}
+
 
     }
 ?>
