7dd7f9288ad75f9300c95e70f11f3ca00dc727b7
Erica Cha Vang <laujmimna@Ericas-MacBook-Air.local>
2015-03-24 21:50:11 -0700

    The testSetId pass.

--- tests/StudentTest.php
+++ tests/StudentTest.php
@@ -44,14 +44,39 @@
             $this->assertEquals("Cool.",$result);
 
         }
-      //
-      //   function testGetId()
-      //
-      // {
-      //   //Arrange
-      //
-      // }
 
+        function testGetId()
+        {
+          //Arrange
+          $name = "Tommy";
+          $date = 2000-03-23;
+          $id = 1;
+          $test_student = new Student($name, $date, $id);
+
+          //Act
+          $result=$test_student->getId();
+
+          //Assert
+          $this->assertEquals(1, $result);
+        }
+
+        function testSetId()
+        {
+          //Arrange
+          $name = "Tommy";
+          $date = 2000-03-23;
+          $id = 1;
+          $test_student = new Student($name, $date, $id);
+
+          //Act
+          $test_student->setId(2);
+
+          //Assert
+          $result = $test_student->getId();
+          $this->assertEquals(2,$result);
+
+
+        }
 
 
 
